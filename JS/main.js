92eb4bd7e47149900296b9b2032d874672477f43
adelinelin9 <134444966+adelinelin9@users.noreply.github.com>
2025-03-21 21:38:40 -0600

    updated scrolling top bar

--- JS/main.js
+++ JS/main.js
@@ -293,3 +293,13 @@ closeBtn.addEventListener("click", () => {
     expandedView.style.display = "none";
     expandedView.classList.remove("show");
 });
+
+document.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
+});
+
